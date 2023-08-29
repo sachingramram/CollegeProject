2a3dfeff3d68f4f74d2ffae7235b91cdbe325a1a
Sachin Pal <sachinpal9352@gmial.com>
2023-08-29 15:18:56 +0530

    reww

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from "react";
+ import {BrowserRouter as Router, Route} from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewLocation from "./locations/pages/NewLocation";
+ const App =() =>{
+    return ( <Router>
+             <Route path ="/">
+                <Users />
+             </Route>
+             <Route path="/locations/new">
+              <NewLocation/>
+             </Route>
+             
+    </Router>
+    );
+ };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ export default App;
---
+++ src/locations/pages/NewLocation.js
@@ -0,0 +1,8 @@
+import React from "react";
+const NewLocation =()=>{
+    return ( 
+        <h1>Location Page</h1>  
+    )
+};
+
+export default NewLocation;
