20a393054e477a85034ea06b1fae4546d220a617
Sachin Pal <sachinpal9352@gmial.com>
2023-09-04 15:50:44 +0530

    pure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
- import React from "react";
+ import React, {useState, useCallback} from "react";
  import {BrowserRouter as Router, Route,Redirect,Switch} from "react-router-dom";
  import Users from "./users/pages/Users";
  import NewLocation from "./locations/pages/NewLocation";
  import MainNavigation from "./common/components/Navigation/MainNavigation";
  import UserLocations from "./locations/pages/UserLocations";
+ import Login from "./users/pages/Login";
+ import { LoginContext } from "./common/components/context";
  const App =() =>{
-    return ( 
-             <Router>
-               <MainNavigation/>
-               <main>
-               <Switch>
-             <Route path ="/" exact>
+  const [isloggedin, setIsloggedin] =useState(false);
+  const login=useCallback(()=>{
+    setIsloggedin(true);
+  },[]);
+
+  const logout=useCallback(()=>{
+    setIsloggedin(false);
+
+  },[]);
+  let validroutes;
+
+  if(isloggedin){
+    validroutes=(
+      <Switch>
+         <Route path ="/" exact>
                 <Users />
              </Route>
              <Route path="/:userid/locations">
@@ -20,9 +31,35 @@
               <NewLocation/>
              </Route>
              <Redirect to="/" />
-             </Switch>
+      </Switch>
+
+    );
+    
+  }
+  else{
+    validroutes=(
+      <Switch>
+        <Route path ="/" exact>
+                <Users />
+             </Route>
+        <Route path="/login" exact>  
+            <Login/>
+           </Route>
+           <Redirect to="/login" />
+      </Switch>
+    )
+  }
+
+    return ( 
+      <LoginContext.Provider value={{isLoggedIn:isloggedin, login: login, logout: logout}}>
+             <Router>
+               <MainNavigation/>
+               <main>
+                {validroutes}
              </main>
     </Router>
+    </LoginContext.Provider>
+
     );
  };
 
